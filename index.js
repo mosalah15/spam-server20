a821ec7754315485f8c4b2b8a4d81c34240e55ae
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-12-06 17:35:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -48,9 +48,5 @@ if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('
    message.channel.sendMessage(args.join("  "))
   }
  });
-client2.on('message', message => {
-    if(message.content === '-ريب'){
-        message.channel.send('#rip @MoDAMer')
-    }
-});
+
 client.login(process.env.BOT_TOKEN);
