6a2f61b5d83a74f7a84c4cac595e66dc28911341
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-12-06 16:46:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -45,4 +45,19 @@ client.on("message", function(message) {
         message.channel.send(args.slice(1, args.length).join(" "))
     }
 });
+var prefix = "5";
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "say") {
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
+   message.channel.sendMessage(args.join("  "))
+  }
+ });
 client.login(process.env.BOT_TOKEN);
