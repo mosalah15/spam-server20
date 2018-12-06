a4021b4902a553d48aaa1682567e31eca77cb919
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-12-06 13:10:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -45,4 +45,19 @@ client.on("message", function(message) {
         message.channel.send(args.slice(1, args.length).join(" "))
     }
 });
+client.on('message', async msg => {
+ 
+if(!devs.includes(message.author.id)) return;
+  if(msg.author.bot) return;
+  let prefix = "11";
+  if(!msg.content.startsWith(prefix)) return;
+  let command = msg.content.split(" ")[0].slice(prefix.length);
+  let args = msg.content.split(" ").slice(1);
+ 
+  if(command == 'say') {
+    if(!args.join(" ")) return msg.delete();
+    msg.channel.send(args.join(" "));
+    return;
+  }
+});
 client.login(process.env.BOT_TOKEN);
