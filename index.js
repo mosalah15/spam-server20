bb46434e46c7f47b3ed8d839ced79149d181eac0
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2019-05-01 08:47:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -80,5 +80,51 @@ const voiceChannel = msg.member.voiceChannel;
             return handleVideo(video, msg, voiceChannel);
   }
 });	 
+client.on('ready', message=> {
+  let command = message.content.split(" ")[0];
+  let args = message.content.split(" ").slice(1);
 
+  if (command == ('join')) {
+function Play(connection, message);
+{
+    var server = servers[message.guild.id]
+    server.dispatcher = connection.playStream(ytdl(server.queue[0], {filter: "audioonly"}))
+    server.queue.shift();
+    server.dispatcher.on("end", function(){
+        if(server.queue[0])
+        {
+            Play(connection,message);
+        }
+        else
+        {
+            connection.disconnect();
+        }
+    })
+}
+
+async run(message, args)
+{
+    if(message.member.voiceChannel)
+    {
+        if(!message.guild.voiceConnection)
+        {
+            if(!servers[message.guild.id])
+            servers[message.guild.id] = {queue: []}
+            }
+            message.member.voiceChannel.join()
+            .then (connection =>{
+                var server = servers[message.guild.id];
+                server.queue.push(args);
+                message.reply('successfly added');
+                Play (connection, message);
+            })
+        }
+    }
+    else
+    {
+        message.reply('join the ch frist')
+    }
+}
+})
+});
 client.login(process.env.BOT_TOKEN);
